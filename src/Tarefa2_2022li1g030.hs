dc227bd3a9735b886fa8ab09bba5af2454a945eb
a72481 <a72481@alunos.uminho.pt>
2022-11-12 23:57:33 +0000

    Adicionados testes da Tarefa 2, efetuadas pequenas alterações nos testes da Tarefa 4 e na documentação da Tarefa 2

--- src/Tarefa2_2022li1g030.hs
+++ src/Tarefa2_2022li1g030.hs
@@ -12,14 +12,14 @@ import LI12223
 
 import System.Random
 
-
+-- | A função 'proximosTerrenosValidos' calcula quais os terrenos passíveis de ser utilizados numa próxima linha do mapa
 proximosTerrenosValidos :: Mapa -> [Terreno]
 proximosTerrenosValidos (Mapa _ ((Rio _, _):(Rio _, _):(Rio _, _):(Rio _, _):t)) = [Estrada 0, Relva]
 proximosTerrenosValidos (Mapa _ ((Estrada _, _):(Estrada _, _):(Estrada _, _):(Estrada _, _):(Estrada _, _):t)) = [Rio 0, Relva]
 proximosTerrenosValidos (Mapa _ ((Relva, _):(Relva, _):(Relva, _):(Relva, _):(Relva, _):t)) = [Rio 0, Estrada 0]
 proximosTerrenosValidos _ = [Rio 0, Estrada 0, Relva]
 
-
+-- | Calcula que obstáculos podem ser utilizados para preencher a lista de obstáculos de uma próxima linha do mapa
 proximosObstaculosValidos :: Int -> (Terreno, [Obstaculo]) -> [Obstaculo]
 proximosObstaculosValidos n (t,l)
     | length l >= n = []
@@ -28,26 +28,31 @@ proximosObstaculosValidos n (t,l)
                     (Estrada _) -> [Nenhum, Carro]
                     Relva -> [Nenhum, Arvore]
 
-
+-- | Função que gera números inteiros aleatoriamente, para serem utilizados em funções posteriores
 gera :: Int -> [Int] -> [Int]
 gera seed lt = let ls = take (length lt) $ randoms (mkStdGen seed)
                    l = zip lt ls
                in map (\(t,s) -> mod s t) l
 
+-- | Dá, com base em aleatórios devolvidos pela função 'gera', uma lista de obstáculos que será usada numa próxima linha do mapa
 geraObstaculo :: [Int] -> [Obstaculo] -> [Obstaculo]
 geraObstaculo l1 l2 = [l2 !! x | x <- l1]
 
+-- | Dá, com base em aleatórios devolvidos pela função 'gera', um terreno que será utilizado numa próxima linha do mapa
 geraTerreno :: Int -> [Terreno] -> Terreno
 geraTerreno x l = l !! x
 
+-- | Dá, com base em aleatórios devolvidos pela função 'gera', uma velocidade que será associada a um terreno numa próxima linha do mapa
 geraVelocidade :: Int -> [Velocidade] -> Velocidade
 geraVelocidade x l = l !! x
 
+-- | Associa a velocidade devolvida pela função geraVelocidade 
 adicionaVelocidade :: Terreno -> Velocidade -> Terreno
 adicionaVelocidade (Rio _) v = Rio v
 adicionaVelocidade (Estrada _) v = Estrada v
 adicionaVelocidade Relva _ = Relva
 
+-- | acrescenta uma linha a um dado mapa
 estendeMapa :: Mapa -> Int -> Mapa
 estendeMapa (Mapa n l) seed = let ptv = proximosTerrenosValidos (Mapa n l)
                                   t0 = geraTerreno (head (gera seed [length ptv])) ptv
--- tests/Tarefa2_2022li1g030_Spec.hs
+++ tests/Tarefa2_2022li1g030_Spec.hs
@@ -5,4 +5,47 @@ import Tarefa2_2022li1g030
 import Test.HUnit
 
 testsT2 :: Test
-testsT2 = TestLabel "Testes Tarefa 2" $ test ["Teste 1" ~: 1 ~=? 1]
+testsT2 = TestLabel "Testes Tarefa 2" $ test [testList1]
+
+takeList :: Mapa -> [(Terreno,[Obstaculo])]
+takeList (Mapa n l) = l
+
+
+-- mapa inicial lista vazia
+m1 = Mapa 4 l1
+l1 = []
+
+-- mapa inicial lista preenchida
+m2 = Mapa 4 l2
+l2 = [(Estrada 3, [Nenhum, Carro, Carro, Nenhum])
+     ,(Rio (-1), [Tronco, Tronco, Nenhum, Nenhum])
+     ,(Relva, [Nenhum, Arvore, Nenhum, Arvore])
+     ,(Relva, [Nenhum, Nenhum, Nenhum, Nenhum])]
+
+-- mapa em que a próxima linha deve ter um terreno diferente de (Rio _)
+m3 = Mapa 3 [(Rio 2, [Nenhum, Tronco, Tronco])
+            ,(Rio (-1), [Tronco, Nenhum, Nenhum])
+            ,(Rio 1, [Nenhum, Tronco, Nenhum])
+            ,(Rio (-2), [Tronco, Tronco, Nenhum])]
+
+
+-- mapa em que a próxima linha deve ter um terreno diferente de (Estrada _)
+m4 = Mapa 5 [(Estrada 2, [Nenhum, Nenhum, Carro, Nenhum, Nenhum])
+            ,(Estrada (-1), [Nenhum, Carro, Carro, Carro, Nenhum])
+            ,(Estrada (-3), [Nenhum, Nenhum, Nenhum, Carro, Carro])
+            ,(Estrada 4, [Nenhum, Carro, Nenhum, Nenhum, Nenhum])
+            ,(Estrada (-2), [Carro, Carro, Nenhum, Nenhum, Carro])]
+
+
+-- mapa em que a próxima linha deve ter um terreno diferente de (Relva)
+m5 = Mapa 4 [(Relva, [Nenhum, Arvore, Nenhum, Nenhum])
+            ,(Relva, [Arvore, Nenhum, Nenhum, Nenhum])
+            ,(Relva, [Nenhum, Nenhum, Arvore, Nenhum])
+            ,(Relva, [Nenhum, Arvore, Nenhum, Nenhum])
+            ,(Relva, [Nenhum, Nenhum, Nenhum, Arvore])]
+
+
+testList1 = "Testes função estendeMapa" ~: test [test1, test2]
+
+test1 = "Teste 1: acrescenta uma linha à lista vazia" ~: (length l1) + 1 ~=? length (takeList (estendeMapa m1 5))
+test2 = "Teste 2: acrescenta uma linha à lista com elementos" ~: (length l2) + 1 ~=? length (takeList (estendeMapa m2 4))
