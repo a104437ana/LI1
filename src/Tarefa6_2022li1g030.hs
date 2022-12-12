c4177010813c994fa3b6a714186ddd3123cefccf
a72481 <a72481@alunos.uminho.pt>
2022-12-12 09:51:08 +0000

    Adicionado ficheiro da Tarefa 6

---
+++ src/Tarefa6_2022li1g030.hs
@@ -0,0 +1,17 @@
+{- |
+Module      : Tarefa6_2022li1g030
+Description : Aplicação gráfica completa
+Copyright   : Ana Sá Oliveira <a104437@alunos.uminho.pt>
+              Sara Campos Ramalho <a72481@alunos.uminho.pt>
+
+Módulo para a realização da Tarefa 6 do projeto de LI1 em 2022/23.
+-}
+module Tarefa6_2022li1g030 where
+
+import LI12223
+import Tarefa1_2022li1g030
+import Tarefa2_2022li1g030
+import Tarefa3_2022li1g030
+import Tarefa4_2022li1g030
+import Tarefa5_2022li1g030
+
