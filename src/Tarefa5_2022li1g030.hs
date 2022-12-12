870e1f9cf9ad82fdd19630286ed5df217ee35d64
a72481 <a72481@alunos.uminho.pt>
2022-12-12 09:13:31 +0000

    Adicionado ficheiro da Tarefa 5

---
+++ src/Tarefa5_2022li1g030.hs
@@ -0,0 +1,19 @@
+{- |
+Module      : Tarefa5_2022li1g030
+Description : Realizar o deslize do mapa
+Copyright   : Ana Sá Oliveira <a104437@alunos.uminho.pt>
+              Sara Campos Ramalho <a72481@alunos.uminho.pt>
+
+Módulo para a realização da Tarefa 5 do projeto de LI1 em 2022/23.
+-}
+module Tarefa5_2022li1g030 (
+-- * Função deslizaJogo
+-- ** Função principal
+deslizaJogo
+-- ** Funções auxiliares
+) where
+
+import LI12223
+
+deslizaJogo :: Jogo -> Jogo
+deslizaJogo = undefined
