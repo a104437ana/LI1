35e1ab21d9772b9a51f3579f1668102ef57d976b
a104437 <a104437@alunos.uminho.pt>
2022-12-19 22:02:30 +0000

    Adicionado código no ficheiro Main

--- src/Main.hs
+++ src/Main.hs
@@ -7,6 +7,10 @@ import Tarefa3_2022li1g030
 import Tarefa4_2022li1g030
 import Tarefa5_2022li1g030
 import Tarefa6_2022li1g030
+import Graphics.Gloss
+import Graphics.Gloss.Interface.Pure.Game
+import Graphics.Gloss.Data.Picture
 
 main :: IO ()
-main = display window black terrenos
+main = do 
+    play janela black 25 estadoInicial desenha evento tempo
