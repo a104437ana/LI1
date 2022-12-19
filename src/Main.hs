df12c037a07181f6a8caebc60360eea34d748995
a104437 <a104437@alunos.uminho.pt>
2022-12-19 15:43:48 +0000

    Adicionado algum código no ficheiro Main

--- src/Main.hs
+++ src/Main.hs
@@ -5,6 +5,8 @@ import Tarefa1_2022li1g030
 import Tarefa2_2022li1g030
 import Tarefa3_2022li1g030
 import Tarefa4_2022li1g030
+import Tarefa5_2022li1g030
+import Tarefa6_2022li1g030
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = display window black terrenos
