11a936fa32ac9e84e55e362c0486a59398013096
a104437 <a104437@alunos.uminho.pt>
2022-12-20 18:32:42 +0000

    Adicionado mais código na Tarefa 6

--- src/Tarefa6_2022li1g030.hs
+++ src/Tarefa6_2022li1g030.hs
@@ -31,29 +31,46 @@ janela :: Display
 janela = InWindow "Crossy Road" (1600,900) (0,0)
 
 estadoInicial :: World
-estadoInicial = (Opcoes Jogar, (Jogo (Jogador (8,8)) (Mapa 16 [])))
+estadoInicial = (Opcoes Jogar, (Jogo (Jogador (8,8)) (Mapa 16 [(Relva,[Arvore,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum]),(Relva,[Arvore,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum]),(Relva,[Arvore,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum]),(Relva,[Arvore,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum]),(Relva,[Arvore,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum]),(Relva,[Arvore,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum]),(Relva,[Arvore,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum]),(Relva,[Arvore,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum])])))
 
 desenha :: World -> Picture
 desenha (Opcoes Jogar, _) = Pictures [fundo,color blue jogar,sair]
 desenha (Opcoes Sair, _) = Pictures [fundo,jogar,color blue sair]
-desenha (ModoJogo, jogo) = desenhaJogo (jogo)
+desenha (ModoJogo, Jogo (Jogador (x,y)) (Mapa 16 l)) = Pictures ((desenhaMapa 0 (Mapa 16 l)) ++ (desenhaJogador x y))
 desenha (PerdeuJogo, _) = Pictures [color red fundo,perdeu]
 
-desenhaJogo :: Jogo -> Picture
-desenhaJogo (Jogo (Jogador (x,y)) (Mapa n l)) = 
+desenhaMapa :: Int -> Mapa -> [Picture]
+desenhaMapa n (Mapa 16 (h:t)) = [desenhaTerreno n h] ++ (desenhaObstaculo 0 n h) ++ (desenhaMapa (n+1) (Mapa 16 t))
+desenhaMapa n (Mapa 16 []) = []
 
-evento :: Event -> World -> Maybe World
-evento (EventKey (SpecialKey KeyEnter) Down _ _) (Opcoes Jogar, _) = Just (ModoJogo, primeiroJogo) -- primeiroJogo é uma função que devolve o jogo com o mapa composto pelas 3 primeiras linhas pré-definidas e as 6 restantes geradas randomly
-evento (EventKey (SpecialKey KeyEnter) Down _ _) (Opcoes Sair, _) = Nothing -- quando chamar esta função, associar o Nothing a fechar a janela
-evento (EventKey (SpecialKey KeyEnter) Down _ _) (PerdeuJogo, _) = Just estadoInicial
-evento (EventKey (SpecialKey KeyUp) Down _ _) (ModoJogo, j) = animaJogo j (Move Cima)
-evento (EventKey (SpecialKey KeyDown) Down _ _) (ModoJogo, j) = animaJogo j (Move Baixo)
-evento (EventKey (SpecialKey KeyLeft) Down _ _) (ModoJogo, j) = animaJogo j (Move Esquerda)
-evento (EventKey (SpecialKey KeyRight) Down _ _) (ModoJogo, j) = animaJogo j (Move Direita)
+desenhaTerreno :: Int -> (Terreno,[Obstaculo]) -> Picture
+desenhaTerreno n (ter,(h:t)) = case ter of Rio _ -> (color azulRio (polygon [((-800),350+(-100)*(fromIntegral n)),((-800),450+(-100)*(fromIntegral n)),(800,450+(-100)*(fromIntegral n)),(800,350+(-100)*(fromIntegral n))]))
+                                           Estrada _ -> (color cinzaEstrada (polygon [((-800),350+(-100)*(fromIntegral n)),((-800),450+(-100)*(fromIntegral n)),(800,450+(-100)*(fromIntegral n)),(800,350+(-100)*(fromIntegral n))]))
+                                           Relva -> (color verdeRelva (polygon [((-800),350+(-100)*(fromIntegral n)),((-800),450+(-100)*(fromIntegral n)),(800,450+(-100)*(fromIntegral n)),(800,350+(-100)*(fromIntegral n))]))
+
+desenhaObstaculo :: Int -> Int -> (Terreno,[Obstaculo]) -> [Picture]
+desenhaObstaculo n1 n2 (ter,[]) = []
+desenhaObstaculo n1 n2 (ter,(h:t)) = case h of Nenhum -> (desenhaObstaculo (n1 + 1) n2 (ter,t))
+                                               Tronco -> [color black (polygon [(((-800)+(100*(fromIntegral n1))),350+(-100)*(fromIntegral n2)),(((-800)+(-100)*(fromIntegral n1)),450+(-100)*(fromIntegral n2)),(((-700)+(100*(fromIntegral n1))),450+(-100)*(fromIntegral n2)),(((-700)+(100*(fromIntegral n1))),350+(-100)*(fromIntegral n2))])] ++ (desenhaObstaculo (n1 + 1) n2 (ter,t))
+                                               Carro -> [color red (polygon [(((-800)+(100*(fromIntegral n1))),350+(-100)*(fromIntegral n2)),(((-800)+(-100)*(fromIntegral n1)),450+(-100)*(fromIntegral n2)),(((-700)+(100*(fromIntegral n1))),450+(-100)*(fromIntegral n2)),(((-700)+(100*(fromIntegral n1))),350+(-100)*(fromIntegral n2))])] ++ (desenhaObstaculo (n1 + 1) n2 (ter,t))
+                                               Arvore -> [color green (polygon [(((-800)+(100*(fromIntegral n1))),350+(-100)*(fromIntegral n2)),(((-800)+(-100)*(fromIntegral n1)),450+(-100)*(fromIntegral n2)),(((-700)+(100*(fromIntegral n1))),450+(-100)*(fromIntegral n2)),(((-700)+(100*(fromIntegral n1))),350+(-100)*(fromIntegral n2))])] ++ (desenhaObstaculo (n1 + 1) n2 (ter,t))
+
+desenhaJogador :: Int -> Int -> [Picture]
+desenhaJogador x y = [color yellow (polygon [(((-800)+(100*(fromIntegral x))),350+(-100)*(fromIntegral y)),(((-800)+(-100)*(fromIntegral x)),450+(-100)*(fromIntegral y)),(((-700)+(100*(fromIntegral x))),450+(-100)*(fromIntegral y)),(((-700)+(100*(fromIntegral x))),350+(-100)*(fromIntegral y))])]
+
+evento :: Event -> World -> World
+evento (EventKey (SpecialKey KeyEnter) Down _ _) (Opcoes Jogar, j) = (ModoJogo, j) -- primeiroJogo é uma função que devolve o jogo com o mapa composto pelas 3 primeiras linhas pré-definidas e as 6 restantes geradas randomly
+evento (EventKey (SpecialKey KeyEnter) Down _ _) (Opcoes Sair, j) =  error "Fim de Jogo"-- quando chamar esta função, associar o Nothing a fechar a janela
+evento (EventKey (SpecialKey KeyEnter) Down _ _) (PerdeuJogo, j) = estadoInicial
+evento (EventKey (SpecialKey KeyUp) Down _ _) (ModoJogo, j) = (ModoJogo, animaJogo j (Move Cima))
+evento (EventKey (SpecialKey KeyDown) Down _ _) (ModoJogo, j) = (ModoJogo, animaJogo j (Move Baixo))
+evento (EventKey (SpecialKey KeyLeft) Down _ _) (ModoJogo, j) = (ModoJogo, animaJogo j (Move Esquerda))
+evento (EventKey (SpecialKey KeyRight) Down _ _) (ModoJogo, j) = (ModoJogo, animaJogo j (Move Direita))
 evento _ s = s  -- ignora qualquer outro evento
 
 tempo :: Float -> World -> World
-tempo 15 (ModoJogo, (Jogo (Jogador (x,y)) (Mapa n l))) = (ModoJogo, deslizaJogo y (Jogo (Jogador (x,y))))
+tempo 15 (ModoJogo, (Jogo (Jogador (x,y)) (Mapa n l))) = (ModoJogo, deslizaJogo y (Jogo (Jogador (x,y)) (Mapa n l)))
+tempo _ w = w
 
 verdeRelva :: Color
 verdeRelva = light green
@@ -64,6 +81,7 @@ cinzaEstrada = greyN 0.8
 azulRio :: Color
 azulRio = light azure
 
+{-
 terreno1 :: Picture
 terreno1 = color verdeRelva (polygon [((-800),(-450)),((-800),(-350)),(800,(-350)),(800,(-450))])
 
@@ -96,15 +114,16 @@ obstaculo11 = color red (polygon [(100,(-450)),(100,(-350)),(0,(-350)),(0,(-450)
 
 terrenos :: Picture
 terrenos = pictures [terreno1,terreno2,terreno3,terreno4,terreno5,terreno6,terreno7,terreno8,terreno9,obstaculo11]
+-}
 
 fundo :: Picture
-fundo = color white (polygon [((-800),(-450)),((-800),450),(800,450),((-800),450)])
+fundo = color white (polygon [((-800),(-450)),((-800),450),(800,450),(800,(-450))])
 
 jogar :: Picture
-jogar = color black (text "Jogar")
+jogar = translate 0 150 (color black (text "Jogar"))
 
 sair :: Picture
-sair = color black (text "Sair")
+sair = translate 0 (-150) (color black (text "Sair"))
 
 perdeu :: Picture
 perdeu = color black (text "Perdeu")
